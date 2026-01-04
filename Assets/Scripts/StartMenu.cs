45a0957032fde06a1ab2312f324b5866263f98c5
Yonatan Ninio <yonatan.n@proton.me>
2026-01-04 22:43:45 +0200

    placeholder buttons with script

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] Button start;
+    [SerializeField] Button options;
+    [SerializeField] Button quit;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (start != null)
+        {
+            start.onClick.AddListener(startGame);
+        }
+        if (options != null)
+        {
+            options.onClick.AddListener(OpenOptions);
+        }
+        if (quit != null)
+        {
+            quit.onClick.AddListener(QuitGame);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void startGame()
+    {
+        Debug.Log("game start");
+    }
+    void OpenOptions()
+    {
+        Debug.Log("options");
+    }
+
+
+    void QuitGame()
+    {
+        Application.Quit();
     }
 }
