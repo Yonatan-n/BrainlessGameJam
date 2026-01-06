ba5fae7cb1cdea1a306953f2e152e40fb8076c06
Yonatan Ninio <yonatan.n@proton.me>
2026-01-06 18:23:29 +0200

    multiple sound by index

--- Assets/Scripts/PhonePad.cs
+++ Assets/Scripts/PhonePad.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PhonePad : MonoBehaviour
 {
+    private List<string> getIndexOfButton = new List<string>{
+    "Pad 1",
+    "Pad 2",
+    "Pad 3",
+    "Pad 4",
+    "Pad 5",
+    "Pad 6",
+    "Pad 7",
+    "Pad 8",
+    "Pad 9",
+    "Pad 0",
+    "Asterisk",
+    "Hashtag",
+    };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +28,7 @@ public class PhonePad : MonoBehaviour
     void soundOnClick()
     {
         PhonePadManager padManagerScript = GetComponentInParent<PhonePadManager>();
-        padManagerScript.PlaySound(0);
+        padManagerScript.PlaySound(getIndexOfButton.IndexOf(gameObject.name));
     }
     // Update is called once per frame
     void Update()
